00b08cecd08a6fb28c39844e4eff4bd60f666126
alexandr <terzisanya4774@gmail.com>
2023-04-12 21:39:04 +0300

    main

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function pow(n, x) {
+    if (x == 0) {
+        return n = 1;
+    } else if (x == 1) {
+        return n = n;
+    } else return n * pow(n, x - 1);
+}
+
+let a = +prompt('Enter number');
+let b = +prompt('Enter degree');
+
+document.write(`Number ${a} to the power of ${b} = `, pow(a, b));
